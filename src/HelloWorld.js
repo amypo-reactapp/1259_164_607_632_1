f1a0a7e68bf6a80583a0ab91398b04d6e5860706
amypo-reactapp <reactapp@amypo.in>
2024-04-02 09:59:15 +0000

    User

---
+++ src/HelloWorld.js
@@ -0,0 +1,16 @@
+// HelloWorld.js
+
+import React from 'react';
+
+// Define a functional component named HelloWorld
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
+}
+
+// Export the component so it can be used elsewhere
+export default HelloWorld;
