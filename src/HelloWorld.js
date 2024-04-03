c15f6283125f1d5a140bc888c99c8b872fbf18c2
amypo-reactapp <reactapp@amypo.in>
2024-04-03 03:00:14 +0000

    User

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -6,7 +6,7 @@ import React from 'react';
 function HelloWorld() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Hello, World!q</h1>
       <p>This is a simple React component.</p>
     </div>
   );
